99e158312b8568c4c3006fb35fef4ad0da7eca42
Josh Karns <jkarns275@gmail.com>
2018-09-13 22:01:01 -0400

    Refactored code + finished project

---
+++ src/facility/worker.rs
@@ -0,0 +1,247 @@
+use std::fmt::Debug;
+use std::fmt::Formatter;
+use std::fmt::Error;
+use std::fmt::Display;
+use std::cmp;
+use facility::affinity::Affinity;
+use rand::Rng;
+use rand::SeedableRng;
+use facility::facility::Facility;
+use std::sync::RwLock;
+use std::sync::Arc;
+use std::sync::atomic::AtomicBool;
+use std::sync::Mutex;
+use std::thread::JoinHandle;
+use std::time::SystemTime;
+use std::sync::mpsc::Sender;
+use std::sync::Barrier;
+use std::thread;
+use std::sync::atomic::Ordering;
+use constants::*;
+
+pub struct Fitness {
+    pub sum: f64,
+    pub grid: Vec<f64>,
+    pub id: usize,
+    pub original: f64,
+}
+
+impl Debug for Fitness {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        write!(f, "(id: {}, f: {:.2}, o: {:.2})", self.id, self.sum, self.original)
+    }
+}
+
+impl Display for Fitness {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        write!(f, "(id: {}, sum: {})", self.id, self.sum)
+    }
+}
+
+impl Eq for Fitness {}
+
+impl PartialEq for Fitness {
+    fn eq(&self, other: &Self) -> bool {
+        self.sum == other.sum
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        self.sum != other.sum
+    }
+}
+
+impl PartialOrd for Fitness {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        self.sum.partial_cmp(&other.sum)
+    }
+}
+
+impl Ord for Fitness {
+    fn cmp(&self, other: &Self) -> cmp::Ordering {
+        match self.partial_cmp(other) {
+            Some(r) => r,
+            None    => cmp::Ordering::Equal,
+        }
+    }
+}
+
+#[allow(unused)]
+pub struct FacilityWorker<T, R>
+    where T: 'static + Affinity + Clone + Send + Sync,
+          R: 'static + Rng + SeedableRng + Send + Sync {
+    /// A shared mutex for the facility. This will be read from and written during the breeding
+    /// process (which is performed by the main thread).
+    pub facility: Arc<RwLock<Facility<T, R>>>,
+
+    /// A boolean flag that, when set to true, will eventually be read by the worker thread - the
+    /// worker thread will then terminate.
+    should_terminate: Arc<AtomicBool>,
+
+    /// A shared container for the fitness to be sent through by the worker thread after an
+    /// iteration.
+    fitness_receiver: Arc<Mutex<Option<Fitness>>>,
+
+    /// Just a thread handle. Will be used to ensure the thread gets terminated gracefully.
+    thread_handle: Option<JoinHandle<()>>,
+
+    /// A unique ID for this worker
+    id: usize
+}
+
+struct Worker<T, R>
+    where T: 'static + Affinity + Clone + Send + Sync,
+          R: 'static + Rng + SeedableRng + Send + Sync {
+    fitness_receiver:   Arc<Mutex<Option<Fitness>>>,
+    facility:           Arc<RwLock<Facility<T, R>>>,
+    should_terminate:   Arc<AtomicBool>,
+    tick_barrier:       Arc<Barrier>,
+    done_sender:        Sender<()>,
+    id: usize,
+}
+
+impl<T, R> Worker<T, R>
+    where T: 'static + Affinity + Clone + Send + Sync,
+          R: 'static + Rng + SeedableRng + Send + Sync {
+
+    pub fn calculate_and_send_fitness(&mut self) {
+        if let Ok(mut f) = self.facility.try_write() {
+            let (original, sum, grid) = f.fitness();
+            if let Ok(mut fitness_recv) = self.fitness_receiver.try_lock() {
+                assert!(fitness_recv.take().is_none());
+                let _ = fitness_recv
+                        .get_or_insert(Fitness { sum, original, grid, id: self.id, });
+            } else {
+                println!("Failed to obtain fitness receiver mutex in thread {}", self.id);
+            }
+        } else {
+            println!("Failed to obtain facility mutex in thread {}", self.id);
+        }
+    }
+
+    pub fn should_terminate(&self) -> bool { (*self.should_terminate).load(Ordering::Relaxed) }
+
+    pub fn do_breeding(&mut self) {
+        let has_parents;
+        if let Ok(f) = self.facility.read() {
+            has_parents = f.parents.is_some();
+        } else {
+            has_parents = false;
+            println!("Failed to obtain facility mutex for breeding in thread {}", self.id);
+        }
+
+        if has_parents {
+            if let Ok(mut f) = self.facility.write() {
+                f.replace_with_child();
+            } else {
+                println!("Failed to obtain facility mutex for breeding in thread {}", self.id);
+            }
+        }
+    }
+
+    pub fn run(mut self) {
+        if let Ok(mut f) = self.facility.try_write() {
+            for _ in 0..N_CHRS {
+                f.gen_and_add_chromosome();
+            }
+        } else {
+            println!("Failed to obtain facility mutex in thread {}", self.id);
+        }
+
+        // Unwrapping is okay-ish here, since the only time this will fail is when the
+        // main thread has panicked
+        self.done_sender.send(()).unwrap(); // Count D
+        self.tick_barrier.wait(); // Sync A
+
+        loop {
+            if self.should_terminate() { return }
+
+            self.calculate_and_send_fitness();
+
+            // Unwrapping here is okay-ish for the same reason as above.
+            self.done_sender.send(()).unwrap(); // Count C
+            // Sync E
+            self.tick_barrier.wait();
+
+            // Parent selection is done in the main thread as of right now...
+            self.do_breeding();
+
+            self.tick_barrier.wait(); // Sync B
+        }
+    }
+}
+
+
+impl<T, R> FacilityWorker<T, R>
+    where T: 'static + Affinity + Clone + Send + Sync,
+          R: 'static + Rng + SeedableRng + Send + Sync {
+
+    pub fn new(id: usize, tick_barrier: Arc<Barrier>, done_sender: Sender<()>,
+               ncols: usize, nrows: usize, data_base: Arc<Vec<T>>) -> Self {
+
+        let now = SystemTime::UNIX_EPOCH;
+        let seed128 = now.elapsed().unwrap().as_nanos() >> id as u128;
+        let mut seed = <R as SeedableRng>::Seed::default();
+        {
+            println!("{}", seed128);
+            use std::mem::transmute;
+            let actual_seed_bytes = seed.as_mut() ;
+            let len = actual_seed_bytes.len();
+            if len > 16 {
+                actual_seed_bytes[0..16]
+                    .clone_from_slice(unsafe { &transmute::<u128, [u8; 16]>(seed128)[..] });
+            } else {
+                actual_seed_bytes[..]
+                    .clone_from_slice(unsafe { &transmute::<u128, [u8; 16]>(seed128)[..len] });
+            }
+        }
+        let facility = Arc::new(RwLock::new(
+            Facility::new(id, ncols, nrows, data_base, R::from_seed(seed))));
+        let should_terminate = Arc::new(AtomicBool::new(false));
+        let fitness_receiver = Arc::new(Mutex::new(None));
+
+        let thread_handle = Some({
+            let should_terminate = should_terminate.clone();
+            let fitness_receiver = fitness_receiver.clone();
+            let facility = facility.clone();
+
+            thread::spawn(move || {
+                Worker {
+                    facility,
+                    should_terminate,
+                    fitness_receiver,
+                    id,
+                    tick_barrier,
+                    done_sender,
+                }.run()
+            })
+        });
+
+        FacilityWorker {
+            facility,
+            should_terminate,
+            thread_handle,
+            fitness_receiver,
+            id
+        }
+    }
+
+    pub fn get_fitness(&mut self) -> Fitness {
+        if let Ok(mut t) = self.fitness_receiver.lock() {
+            t.take().unwrap()
+        } else {
+            panic!("Failed to lock fitness_receiver in thread {}", self.id)
+        }
+    }
+
+    pub fn set_to_terminate(&mut self) {
+        self.should_terminate.store(true, Ordering::Relaxed);
+    }
+
+    pub fn join(mut self) -> Result<(), ()> {
+        let thread_handle = self.thread_handle.take().unwrap();
+        match thread_handle.join() {
+            Ok(())  => Ok(()),
+            Err(_)  => Err(())
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@
 #![feature(int_to_from_bytes)]
 #![feature(try_from)]
 
+
 extern crate rand;
 extern crate core;
+extern crate pancurses;
+use pancurses::*;
 
 pub mod facility;
 pub mod constants;
@@ -16,7 +19,25 @@ use std::sync::{Arc, Barrier};
 use rand::{Rng, XorShiftRng};
 
 fn main() {
+    type Affine = (f64, f64, f64);
 
+    const CHARSET: [char; 6] = ['-', '~', '#', '$', '!', '█'];
+    const COLORS: [i16; 6] = [0, 1, 2, 3, 4, 5];
+
+    let affine_max = <Affine as Affinity>::max() * 4.0;
+
+    let window = initscr();
+    noecho();
+    window.keypad(false);
+    window.nodelay(true);
+    start_color();
+
+    init_pair(0, COLOR_BLACK, COLOR_BLACK );
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(3, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(4, COLOR_CYAN, COLOR_BLACK);
+    init_pair(5, COLOR_GREEN, COLOR_BLACK);
 
     use std::time::SystemTime;
     use std::mem::transmute;
@@ -24,18 +45,19 @@ fn main() {
 
     let seed = SystemTime::UNIX_EPOCH.elapsed().unwrap().as_nanos();
     let rng = XorShiftRng::from_seed(unsafe { transmute::<u128, [u8; 16]>(seed) });
-    let data = Arc::new(Facility::<f64, XorShiftRng>::gen_random_data(N_COLS, N_ROWS, rng));
+    let data = Arc::new(Facility::<Affine, XorShiftRng>::gen_random_data(N_COLS, N_ROWS, rng));
     let (tx, rx) = channel::<()>();
 
     let barrier = Arc::new(Barrier::new(N_THREADS + 1));
     let mut workers = (0..N_THREADS).map(|id| {
-        FacilityWorker::<f64, XorShiftRng>::new(id, N_CHRS, barrier.clone(), tx.clone(), N_COLS,
+        FacilityWorker::<Affine, XorShiftRng>::new(id, barrier.clone(), tx.clone(), N_COLS,
                                                 N_ROWS, data.clone())
     }).collect::<Vec<_>>();
 
     let mut rng = XorShiftRng::from_seed(unsafe { transmute::<u128, [u8; 16]>(seed) });
 
     let mut counter = 0;
+    let mut end = false;
     // Count D
     while counter < N_THREADS {
         match rx.recv() {
@@ -47,53 +69,82 @@ fn main() {
     barrier.wait(); // Sync A
 
     for i in 0..N_GENERATIONS {
-        for iter in 0..N_GENS_PER_ITER {
-            let mut counter = 0;
-            // Count C
-            while counter < N_THREADS {
-                match rx.recv() {
-                    Ok(()) => counter += 1,
-                    Err(_) => panic!("Received error in channel.")
-                }
+        let mut counter = 0;
+        // Count C
+        while counter < N_THREADS {
+            match rx.recv() {
+                Ok(()) => counter += 1,
+                Err(_) => panic!("Received error in channel.")
             }
-
-            // Do breeding
-            let mut weights = workers.iter_mut().map(|w| w.get_fitness()).collect::<Vec<_>>();
-            // Unstable sort is faster according to rustdocs
-            weights.sort_unstable();
-            if iter == N_GENS_PER_ITER - 1 {
-                println!("Fitnesses: {:?}", weights);
-            }
-            // The lowest (first) N_THREADS / 2 will be thrown out. The other half will be selected for
-            // breeding randomly. Every facility of the other half will have at least one child.
-            let partners = (0..N_THREADS - N_PARENTS)
-                .map(|i| (i, N_THREADS - N_PARENTS + (rng.gen::<usize>() % N_PARENTS),
-                          N_THREADS - N_PARENTS + (rng.gen::<usize>() % N_PARENTS)))
-                .map(|(i, a, b)| {
-                    if a == b { (i, a, N_THREADS - N_PARENTS + ((b + 1) % N_PARENTS)) } else { (i, a, b) }
-                })
-                .map(|(i, a, b)| {
-                    let (ind, inda, indb) = (weights[i].id, weights[a].id, weights[b].id);
-                    let parenta = workers[inda].facility.clone();
-                    let parentb = workers[indb].facility.clone();
-                    let child = workers[ind].facility.clone();
-                    if let Ok(mut c) = workers[ind].facility.try_write() {
-                        c.set_parents(parenta, parentb);
-                    } else {
-                        println!("Failed to obtain parenta lock of thread {}", inda);
-                    }
-                })
-                .count();
-
-            // Sync E - after this call to wait the actual breeding will occur in the worker threads
-            barrier.wait();
-            // Sync F
-            barrier.wait();
         }
 
+        // Do breeding
+        let mut weights = workers.iter_mut().map(|w| w.get_fitness()).collect::<Vec<_>>();
+        // Unstable sort is faster according to rustdocs
+        weights.sort_unstable();
+
+        // println!("Fitnesses: {:?}", weights);
+
+        // The lowest (first) N_THREADS / 2 will be thrown out. The other half will be selected for
+        // breeding randomly. Every facility of the other half will have at least one child.
+        let _ = (0..N_THREADS - N_PARENTS)
+            .map(|i| (i, N_THREADS - N_PARENTS + (rng.gen::<usize>() % N_PARENTS),
+                      N_THREADS - N_PARENTS + (rng.gen::<usize>() % N_PARENTS)))
+            .map(|(i, a, b)| {
+                if a == b { (i, a, N_THREADS - N_PARENTS + ((b + 1) % N_PARENTS)) } else { (i, a, b) }
+            })
+            .map(|(i, a, b)| {
+                let (ind, inda, indb) = (weights[i].id, weights[a].id, weights[b].id);
+                let parenta = workers[inda].facility.clone();
+                let parentb = workers[indb].facility.clone();
+                if let Ok(mut c) = workers[ind].facility.try_write() {
+                    c.set_parents(parenta, parentb);
+                } else {
+                    println!("Failed to obtain parenta lock of thread {}", inda);
+                }
+            })
+            .count();
+        let max_fitness = weights.pop().unwrap();
+        // Sync E - after this call to wait the actual breeding will occur in the worker threads
+        barrier.wait();
+
+        fn index(col: usize, row: usize) -> usize {
+            col * N_ROWS + row
+        }
+
+        let aff_to_chclr = |aff: f64| -> (char, i16) {
+            let aff = ((aff / affine_max) * 10.0) as usize;
+            if aff >= 6 {
+                (CHARSET[5], COLORS[5])
+            } else {
+                (CHARSET[aff], COLORS[aff])
+            }
+        };
+
+        for row in 0..N_ROWS {
+            window.mv(row as i32, 0);
+            for col in 0..N_COLS {
+                let (_ch, clr) = aff_to_chclr(max_fitness.grid[index(col, row)]);
+                window.color_set(clr);
+                window.addch('█');
+            }
+        }
+        window.color_set(2);
+        let df = max_fitness.sum - max_fitness.original;
+        if df > 0.0 {
+            window.printw(format!("\nΔ fitness: +{:.3}", max_fitness.sum - max_fitness.original));
+        } else {
+            window.printw(format!("\nΔ fitness: {:.3}", max_fitness.sum - max_fitness.original));
+        }
+        window.printw(format!("\n actual fitness: {:.3}", max_fitness.sum));
+        match window.getch() {
+            Some(Input::Character(x)) if x == ' ' => end = true,
+            _ => {}
+        }
+        window.refresh();
         // Update display.
         // TODO: actually have a display. Possible just display the "most fit" grid
-
+        if end { break }
         // If we're on the last iteration, we want to set all the the threads to terminate before we
         // synchronize with them and they continue on the the next iteration.
         if i < N_GENERATIONS - 1 {
@@ -108,8 +159,10 @@ fn main() {
     // Sync B
     barrier.wait();
 
-    let facilities = workers
+    let _ = workers
         .into_iter()
         .map(|w| w.join().unwrap())
-        .collect::<Vec<_>>();
+        .count();
+
+    endwin();
 }
