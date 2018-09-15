ecdf31c60e91b292d48ae8d033f2f940030c298f
Josh Karns <jkarns275@gmail.com>
2018-09-15 11:43:25 -0400

    refactored

--- src/constants.rs
+++ src/constants.rs
@@ -1,10 +1,10 @@
-pub const N_THREADS: usize = 8;
+pub const N_THREADS: usize = 4;
 pub const N_THREADS_2: usize = N_THREADS / 2;
-pub const N_GENERATIONS: usize = 1024 * 1024;
-pub const N_COLS: usize = 64;
+pub const N_GENERATIONS: usize = 1024 * 1024 * 1024;
+pub const N_COLS: usize = 32;
 pub const N_ROWS: usize = 32;
 
 pub const N_CHRS: usize = 5;
 pub const N_PARENTS: usize = 2;
-pub const MUTATION_COEFFICIENT: f64 = 0.00001;
+pub const MUTATION_COEFFICIENT: f64 = 0.001;
 pub const N_GENS_PER_ITER: usize = 8;
