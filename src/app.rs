28ac6e896cee67c7c84d7d9de2f41ab80359d46d
Joshua Karns <jkarns275@gmail.com>
2018-09-28 16:06:21 -0400

    oof

--- src/app.rs
+++ src/app.rs
@@ -30,7 +30,7 @@ pub struct App<T, R> where T: 'static + Affinity + Clone + Send + Sync,
 impl<T, R> App<T, R> where  T: Affinity + Clone + Send + Sync,
                             R: Rng + SeedableRng + Send + Sync {
 
-    const MS_PER_ITER_UPDATE_FREQ: usize = 256;
+    const MS_PER_ITER_UPDATE_FREQ: usize = 4;
 
     pub fn new() -> Self {
         let window = App::<T, R>::init_window();
@@ -39,8 +39,8 @@ impl<T, R> App<T, R> where  T: Affinity + Clone + Send + Sync,
         let mut data = Facility::<T, XorShiftRng, CircleWeightGen>::gen_random_data(N_COLS, N_ROWS, rng);
 
         let circle = CircleWeightGen {
-            inner_radius: 20.5,
-            outer_radius: 24.0,
+            inner_radius: 10.5,
+            outer_radius: 14.0,
             position: ((N_COLS / 2) as f64, (N_ROWS / 2) as f64),
             internal_weight: 2.0,
             external_weight: 0.4,
--- src/constants.rs
+++ src/constants.rs
@@ -1,13 +1,13 @@
-pub const N_THREADS: usize = 8;
+pub const N_THREADS: usize = 60;
 pub const N_THREADS_2: usize = N_THREADS / 2;
 pub const N_GENERATIONS: usize = !0;
-pub const N_COLS: usize = 128;
+pub const N_COLS: usize = 64;
 pub const N_ROWS: usize = 64;
-pub const N_EMPTY_CELLS: usize = 60 * 128;
+pub const N_EMPTY_CELLS: usize = 64 * 62;
 pub const N_CELLS: usize = (N_COLS * N_ROWS) - N_EMPTY_CELLS;
 
 pub const N_CHRS: usize = 128;
-pub const N_PARENTS: usize = 3;
+pub const N_PARENTS: usize = 7;
 pub const MUTATION_COEFFICIENT: f64 = 0.0025;
-pub const N_GENS_PER_ITER: usize = 1;
+pub const N_GENS_PER_ITER: usize = 64;
 pub const UPDATE_FREQUENCY: usize = 1;
